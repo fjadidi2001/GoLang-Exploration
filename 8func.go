63e66ba99276d0adc7fbfc11609f3eeccab4501c
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-05-21 16:22:26 -0400

    Update 8func.go

--- 8func.go
+++ 8func.go
@@ -1,1 +1,13 @@
 package main
+import "fmt"
+
+func Calc(Sum, Min, Mul, Div float64) (float64, float64) {
+	var area = (Base * Height) / 2
+	var perimeter = side1 + side2 + Base
+	return area, perimeter
+}
+func main() {
+	area, perimeter := calculation(2, 10.2, 10.5, 10)
+	fmt.Println(area, perimeter)
+	fmt.Printf("AREA TYPE:%T,PERIMETER TYPE %T\n", area, perimeter)
+}
