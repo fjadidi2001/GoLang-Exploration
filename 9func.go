112e4858752a034d822d37aeabe6d8ce0472e69d
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-05-25 12:40:19 -0400

    Update 9func.go

--- 9func.go
+++ 9func.go
@@ -11,7 +11,7 @@ func multi(num1, num2 float64) float64 {
 }
 func main() {
 	num1, num2 := 56, 76.82
-	div := divi(float64(num1), num2)
-	fmt.Println("div is :", div)
+	multi := multii(float64(num1), num2)
+	fmt.Println("multi is :", multi)
 
 }
