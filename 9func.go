87f5aa3d4c15149728d5ccc523fca94d40dcd2da
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-05-25 12:40:32 -0400

    Update 9func.go

--- 9func.go
+++ 9func.go
@@ -1,17 +1,1 @@
-package main
 
-import (
-	"fmt"
-)
-
-func multi(num1, num2 float64) float64 {
-	var multi = num1 * num2
-	return multi
-
-}
-func main() {
-	num1, num2 := 56, 76.82
-	multi := multii(float64(num1), num2)
-	fmt.Println("multi is :", multi)
-
-}
