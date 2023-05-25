0c9ff31264e2a2f9fea28fe5ccff3fbfeb7380c3
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-05-25 12:38:50 -0400

    Update 9func.go

--- 9func.go
+++ 9func.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"fmt"
+)
+
+func multi(num1, num2 float64) float64 {
+	var multi = num1 * num2
+	return multi
+
+}
+func main() {
+	num1, num2 := 56, 76.82
+	div := divi(float64(num1), num2)
+	fmt.Println("div is :", div)
+
+}
