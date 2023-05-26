e6a9a62a89a6d51c78766ba71aa8ed6cbccde3d2
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-05-26 16:26:24 -0400

    Update 2func.go

--- 2func.go
+++ 2func.go
@@ -11,3 +11,4 @@ func split(sum int) (x, y int) {
 func main() {
 	fmt.Println(split(17))
 }
+//  Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions. 
