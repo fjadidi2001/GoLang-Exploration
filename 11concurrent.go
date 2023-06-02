2d9e2283a88da9f9e0f12037356b1d81ab7c01ee
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-02 16:03:16 -0400

    Update 11concurrent.go

--- 11concurrent.go
+++ 11concurrent.go
@@ -1,1 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go helloworld(&wg)
+	go goodbye(&wg)
+	wg.Wait()
+}
+
+func helloworld(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Hello World!")
+}
+
+func goodbye(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Good Bye!")
+}
 
