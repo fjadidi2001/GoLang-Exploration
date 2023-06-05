4ea5a747e1566f6031b78e01763a821c2b20d1d8
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-05 16:30:01 -0400

    Update 13infiniteloop.go

--- 13infiniteloop.go
+++ 13infiniteloop.go
@@ -2,9 +2,9 @@
 package main
 import "fmt"
 func main(){
-sum := 0
-for {
-	sum++ // repeated forever
-}
-fmt.Println(sum) // never reached
+	sum := 0
+	for {
+		sum++ // repeated forever
+	}
+	fmt.Println(sum) // never reached
 }
