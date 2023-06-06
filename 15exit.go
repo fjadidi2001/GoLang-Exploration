0212d32a47c81c3c8558623789b904add43ea725
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-06 16:28:51 -0400

    Update 15exit.go

--- 15exit.go
+++ 15exit.go
@@ -1,1 +1,16 @@
 
+package main
+func main(){
+  sum := 0
+  for i := 1; i < 5; i++ {
+	  if i%2 != 0 { // skip odd numbers
+		  continue
+	  }
+	  sum += i
+  }
+  fmt.Println(sum) // 6 (2+4)
+
+
+
+
+}
